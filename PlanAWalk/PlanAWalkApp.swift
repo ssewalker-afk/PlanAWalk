8290366f1f10e8e8fc5e117aa8f6358026584348
ssewalker-afk <ssewalker@yahoo.com>
2026-03-08 20:56:12 -0500

    Initial Commit

---
+++ PlanAWalk/PlanAWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanAWalkApp.swift
+//  PlanAWalk
+//
+//  Created by Sarah Walker on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct PlanAWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
